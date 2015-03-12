3d99ee60620f61d39ed985aa073dbee9b86a0067
imranhakeem <linuxholic@hotmail.com>
2015-03-12 19:16:40 +0500

    Location Method replaced. Fused Location implemented. Added launcher icon.

--- app/src/main/java/com/byteshaft/trackbuddy/LocationService.java
+++ app/src/main/java/com/byteshaft/trackbuddy/LocationService.java
@@ -10,84 +10,25 @@ import android.os.Bundle;
 
 public class LocationService extends ContextWrapper implements LocationListener {
 
-    Location location;
 
     boolean isGPSEnabled = false;
     boolean isNetworkEnabled = false;
-
-    static double latitude;
-    static double longitude;
-
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    private static final long MIN_TIME_BW_UPDATES = 1000;
 
     public static LocationManager locationManager;
 
     public LocationService(Context context) {
         super(context);
-        getLocation();
     }
 
-
-
-    public Location getLocation() {
+    public void isLocationServiceAvailable() {
         try {
             locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
             isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
             isNetworkEnabled = locationManager.isProviderEnabled((LocationManager.NETWORK_PROVIDER));
 
-
-            if (isNetworkEnabled) {
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-                        MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-
-                if (locationManager != null) {
-                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-
-                    if (location != null) {
-                        latitude = location.getLatitude();
-                        longitude = location.getLongitude();
-                    }
-                }
-                if (isGPSEnabled) {
-
-                    if (location == null) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER,
-                                MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                    }
-
-                    if (locationManager != null) {
-                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        System.out.println("Shiii");
-                        if(location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                }
-            }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return location;
-    }
-
-    public double getLatitude() {
-        if(location != null) {
-            latitude = location.getLatitude();
-        }
-        return latitude;
-    }
-    public double getLongitude() {
-        if(location != null) {
-            longitude = location.getLongitude();
-        }
-        return longitude;
     }
 
     @Override
