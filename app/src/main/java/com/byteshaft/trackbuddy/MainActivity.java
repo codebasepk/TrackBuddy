ae7acc6c45c442b99b8695c7853defb1204505b0
Mohsin Falak <fi8er1@gmail.com>
2015-03-12 23:51:02 +0500

    Basic UI implemented. Reply SMS Improved.

--- app/src/main/java/com/byteshaft/trackbuddy/MainActivity.java
+++ app/src/main/java/com/byteshaft/trackbuddy/MainActivity.java
@@ -11,49 +11,18 @@ import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
-    Button sendBTN;
-    Button locationBTN;
-    static EditText mEditText;
-    static EditText mEditText2;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sendBTN = (Button) findViewById(R.id.sendButton);
-        locationBTN = (Button) findViewById(R.id.locationButton);
-        mEditText = (EditText) findViewById(R.id.phoneNumber);
-        mEditText2 = (EditText) findViewById(R.id.smsText);
-        sendBTN.setOnClickListener(this);
-        locationBTN.setOnClickListener(this);
+
 
     }
 
-
-    private void sendSms(String number, String txt) {
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage(number, null, txt, null, null);
-
-    }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.sendButton:
-                sendSms(mEditText.getText().toString(), mEditText2.getText().toString());
-                mEditText.getText().clear();
-                mEditText2.getText().clear();
-//        }
-//        switch (v.getId()) {
-//            case R.id.locationButton:
-//                LocationService gps = new LocationService(getApplicationContext());
-//                gps.getLocation();
-//                //if(gps.canGetLocation){
-//                 latitude = gps.getLatitude();
-//                 longitude = gps.getLongitude();
 
-                }
 
 
         }
