c3ee5a6b3a0e42bb33da03aa89f518ca1f4cc963
Mohsin Falak <fi8er1@gmail.com>
2015-03-19 16:13:25 +0500

    Navigation Drawer properly implemented. Logo Enhanced.

--- app/src/main/java/com/byteshaft/trackbuddy/MainActivity.java
+++ app/src/main/java/com/byteshaft/trackbuddy/MainActivity.java
@@ -1,35 +1,41 @@
 package com.byteshaft.trackbuddy;
 
+import android.app.FragmentManager;
+import android.content.Context;
 import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener{
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
 
     private DrawerLayout drawerLayout;
     private ListView listView;
-    private String[] items;
     private ActionBarDrawerToggle drawerListener;
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        items = getResources().getStringArray(R.array.items);
         listView = (ListView) findViewById(R.id.drawer_list);
-        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items));
+        myAdapter = new MyAdapter(this);
+        listView.setAdapter(myAdapter);
         listView.setOnItemClickListener(this);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerListener = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_close){
+        drawerListener = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_drawer, R.string.drawer_close) {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
@@ -51,7 +57,7 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(drawerListener.onOptionsItemSelected(item)){
+        if (drawerListener.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -71,13 +77,57 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-        Toast.makeText(this, items[position], Toast.LENGTH_LONG).show();
         selectItem(position);
+
+
     }
 
     private void selectItem(int position) {
         listView.setItemChecked(position, true);
     }
-}
 
+
+
+
+    class MyAdapter extends BaseAdapter {
+
+        private Context context;
+        String[] items;
+        int[] images = {R.drawable.ic_tracker, R.drawable.ic_siren, R.drawable.ic_speed, R.drawable.ic_list};
+
+        public MyAdapter(Context context) {
+            this.context = context;
+            items = context.getResources().getStringArray(R.array.items);
+        }
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = null;
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                row = inflater.inflate(R.layout.drawer_layout, parent, false);
+            } else {
+                row = convertView;
+            }
+            ImageView iconImageView = (ImageView) row.findViewById(R.id.trackerIcon);
+            iconImageView.setImageResource(images[position]);
+            return row;
+        }
+    }
+}
