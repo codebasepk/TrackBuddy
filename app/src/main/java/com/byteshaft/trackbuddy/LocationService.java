9e12b6433cc541546e089fabeccf86c2da732ffa
Mohsin Falak <fi8er1@gmail.com>
2015-04-07 19:06:52 +0500

    Location fixed! First Run layout properly implemented.

--- app/src/main/java/com/byteshaft/trackbuddy/LocationService.java
+++ app/src/main/java/com/byteshaft/trackbuddy/LocationService.java
@@ -4,37 +4,44 @@ package com.byteshaft.trackbuddy;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+
 
 public class LocationService extends ContextWrapper implements LocationListener,
         GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    LocationRequest mLocationRequest;
+    Location mLocation;
+
+
     static double speed;
     private GoogleApiClient mGoogleApiClient;
 
-    static LocationManager locationManager;
     private Helper mHelpers = null;
 
     public LocationService(Context context) {
         super(context);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
         mHelpers = new Helper(context);
     }
 
     void acquireLocation() {
-        connectGoogleApiClient();
+        settingUpGoogleApiClient();
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Location mLocation;
+
                 int counter = 0;
                 do {
                     try {
@@ -43,7 +50,7 @@ public class LocationService extends ContextWrapper implements LocationListener,
                         e.printStackTrace();
                     }
 
-                    mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+//                    mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
                     counter++;
                     System.out.println("Tracker Thread Runnning... " + counter);
                     if (counter > 120 && mLocation == null) {
@@ -54,10 +61,14 @@ public class LocationService extends ContextWrapper implements LocationListener,
                     String lat = String.valueOf(mLocation.getLatitude());
                     String lon = String.valueOf(mLocation.getLongitude());
                     mHelpers.sendSms(SMSManager.phoneNumber, "TrackBuddy:\n\nhttps://maps.google.com/maps?q=" + lat + "," + lon);
+                    stopLocationService();
+                    mLocation = null;
                     Log.i("Location", "Location acquired. Sending SMS...");
                 } else {
                     mHelpers.sendSms(SMSManager.phoneNumber, "TrackBuddy:\n\nTarget device cannot be located at the moment.\n\nMake sure the Location Service of the target device is on High-Accuracy Mode.");
                     Log.i("Location", "Device cannot be Located. Sending SMS...");
+                    stopLocationService();
+                    mLocation = null;
                 }
             }
         }).start();
@@ -85,17 +96,17 @@ public class LocationService extends ContextWrapper implements LocationListener,
                     }
 
                 } while (speed == 0.0);
-                int roundedValueSpeed = (int)mSpeed;
-                mHelpers.sendSms(SMSManager.phoneNumber, "TrackBuddy:\n\nCurrent speed of the target device is: " + roundedValueSpeed*3600/1000 + " Km/h\n\n(Accuracy +/- 5 Km/h)");
+                int roundedValueSpeed = (int) mSpeed;
+                mHelpers.sendSms(SMSManager.phoneNumber, "TrackBuddy:\n\nCurrent speed of the target device is: " + roundedValueSpeed * 3600 / 1000 + " Km/h\n\n(Accuracy +/- 5 Km/h)");
                 Log.i("Speed", "Current Speed acquired. Sending SMS...");
-                locationManager.removeUpdates(LocationService.this);
                 speed = 0.0;
 
             }
         }).start();
     }
 
-    private void connectGoogleApiClient() {
+    private void settingUpGoogleApiClient() {
+        createLocationRequest();
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
@@ -104,18 +115,43 @@ public class LocationService extends ContextWrapper implements LocationListener,
         mGoogleApiClient.connect();
     }
 
-    @Override public void onConnectionFailed(ConnectionResult connectionResult) {}
-    @Override public void onConnected(Bundle bundle) {}
-    @Override public void onConnectionSuspended(int i) {}
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    public void startLocationUpdates() {
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    public void stopLocationService() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
 
     @Override
     public void onLocationChanged(Location location) {
+        mLocation = location;
         location.getLatitude();
         speed = location.getSpeed();
     }
-
-    @Override public void onStatusChanged(String provider, int status, Bundle extras) {}
-    @Override public void onProviderEnabled(String provider) {}
-    @Override public void onProviderDisabled(String provider) {}
 }
+
 
